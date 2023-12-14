a5fcc763506d9daa9ec13e3e0eaee45b7fcb7ebd
Adel Kharbout <Adel.Kharbout@ecube.de>
2023-12-14 14:43:29 +0100

    webpack html plugin + style - css loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,39 @@
-const path = require('path')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
+        clean: true,
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
     },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },
+        port: 3000,
+        open: true,//opens in browser
+        hot: true,//loading
+        compress: true,
+        historyApiFallback: true,
+
+
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'filter.html',
+            template: 'src/html/Basics/filter.html'
+        })
+    ]
+
 };
